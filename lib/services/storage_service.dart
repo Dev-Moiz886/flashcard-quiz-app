8a2e16615bb15dcfbb3f9534ef9167f3e670bc82
Dev-Moiz886 <abdulmmoiz886@gmail.com>
2026-07-26 04:02:37 +0500

    Update storage_service.dart
    Refactor saveFlashcards and loadFlashcards methods for better readability.

--- lib/services/storage_service.dart
+++ lib/services/storage_service.dart
@@ -4,23 +4,16 @@ import '../models/flashcard.dart';
 
 class StorageService {
   static const String flashcardKey = "flashcards";
-
   static Future<void> saveFlashcards(List<Flashcard> cards) async {
     final prefs = await SharedPreferences.getInstance();
-
     List<String> data =
         cards.map((card) => jsonEncode(card.toJson())).toList();
-
     await prefs.setStringList(flashcardKey, data);
   }
-
   static Future<List<Flashcard>> loadFlashcards() async {
     final prefs = await SharedPreferences.getInstance();
-
     List<String>? data = prefs.getStringList(flashcardKey);
-
     if (data == null) return [];
-
     return data
         .map(
           (item) => Flashcard.fromJson(
