8e99fee1901e1d839db5bcf1b0c82e49f218ba24
Dev-Moiz886 <abdulmmoiz886@gmail.com>
2026-07-26 03:56:52 +0500

    Update flashcard.dart

--- lib/models/flashcard.dart
+++ lib/models/flashcard.dart
@@ -1,19 +1,16 @@
 class Flashcard {
   String question;
   String answer;
-
   Flashcard({
     required this.question,
     required this.answer,
   });
-
   Map<String, dynamic> toJson() {
     return {
       'question': question,
       'answer': answer,
     };
   }
-
   factory Flashcard.fromJson(Map<String, dynamic> json) {
     return Flashcard(
       question: json['question'],
