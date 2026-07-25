a41d884646004a5111a400824d81fbe68813fda7
Dev-Moiz886 <abdulmmoiz886@gmail.com>
2026-07-26 04:04:26 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -8,29 +8,22 @@ void main() {
 
 class FlashCardQuizApp extends StatelessWidget {
   const FlashCardQuizApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'FlashCard Quiz',
-
       theme: ThemeData(
         useMaterial3: true,
-
         textTheme: GoogleFonts.poppinsTextTheme(),
-
         colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xff6366F1)),
-
         scaffoldBackgroundColor: const Color(0xffF8FAFC),
-
         appBarTheme: const AppBarTheme(
           centerTitle: true,
           backgroundColor: Color(0xff6366F1),
           foregroundColor: Colors.white,
           elevation: 0,
         ),
-
         elevatedButtonTheme: ElevatedButtonThemeData(
           style: ElevatedButton.styleFrom(
             minimumSize: const Size(150, 55),
@@ -39,12 +32,10 @@ class FlashCardQuizApp extends StatelessWidget {
             ),
           ),
         ),
-
         floatingActionButtonTheme: const FloatingActionButtonThemeData(
           backgroundColor: Color(0xff6366F1),
           foregroundColor: Colors.white,
         ),
-
         cardTheme: CardThemeData(
           elevation: 10,
           shape: RoundedRectangleBorder(
@@ -52,7 +43,6 @@ class FlashCardQuizApp extends StatelessWidget {
           ),
         ),
       ),
-
       home: const HomeScreen(),
     );
   }
